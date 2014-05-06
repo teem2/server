4786ea26894923ca9fd889d36ef27f6df9208436
Max Carlson <max@maxcarlson.com>
2014-05-06 02:47:22 -0700

    now with HARD CORE API caching

--- apiproxy.js
+++ apiproxy.js
@@ -13,13 +13,20 @@ module.exports = function (pattern, target, host, port) {
       // strip off query string
       newpath = newpath.replace(/\?.*/, '');
 
+      var query = req.query;
       // rewrite query parameters with ROVI signature
-      req.query.sig = roviSig(process.env.ROVI_SEARCH_KEY, process.env.ROVI_SEARCH_SECRET);
-      req.query.apikey = process.env.ROVI_SEARCH_KEY;
-      req.query.format = 'json'
-      req.url = newpath + '?' + querystring.stringify(req.query);
+      query.sig = roviSig(process.env.ROVI_SEARCH_KEY, process.env.ROVI_SEARCH_SECRET);
+      query.apikey = process.env.ROVI_SEARCH_KEY;
+      query.format = 'json'
+      req.url = newpath + '?' + querystring.stringify(query);
 
       console.log('proxying', url, req.url);
+
+      proxy.on('proxyRes', function (res) {
+        // aggro cache, see https://devcenter.heroku.com/articles/increasing-application-performance-with-http-cache-headers
+        res.headers['Cache-Control'] = 'public, max-age=31536000';
+        res.headers['Expires'] = 'Mon, 25 Jun 2015 21:31:12 GMT';
+      });
 
       proxy.web(req, res, {
         target: 'http://api.rovicorp.com',
