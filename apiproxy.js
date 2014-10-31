b46fb58ceeaf31c14fdceddc682f436e70066418
Max Carlson <max@maxcarlson.com>
2014-10-31 14:52:38 +0900

    Add Rovi API rate limiting to the server

--- apiproxy.js
+++ apiproxy.js
@@ -3,40 +3,44 @@
 var proxy = require('http-proxy').createProxy();
 var querystring = require('querystring');
 var roviSig = require('./node_modules/rovi/lib/sig.js')
+var RateLimiter = require('limiter').RateLimiter;
+var limiter = new RateLimiter(1, 255);
 
 module.exports = function (pattern, target, host, port) {
   return function(req, res, next) {
-    var url = req.url;
-    if (url.match(pattern)) {
-      // strip off leading path
-      var newpath = url.replace(pattern, '/');
-      // strip off query string
-      newpath = newpath.replace(/\?.*/, '');
+    limiter.removeTokens(1, function() {
+      var url = req.url;
+      if (url.match(pattern)) {
+        // strip off leading path
+        var newpath = url.replace(pattern, '/');
+        // strip off query string
+        newpath = newpath.replace(/\?.*/, '');
 
-      var query = req.query;
-      // rewrite query parameters with ROVI signature
-      query.sig = roviSig(process.env.ROVI_SEARCH_KEY, process.env.ROVI_SEARCH_SECRET);
-      query.apikey = process.env.ROVI_SEARCH_KEY;
-      query.format = 'json'
-      req.url = newpath + '?' + querystring.stringify(query);
+        var query = req.query;
+        // rewrite query parameters with ROVI signature
+        query.sig = roviSig(process.env.ROVI_SEARCH_KEY, process.env.ROVI_SEARCH_SECRET);
+        query.apikey = process.env.ROVI_SEARCH_KEY;
+        query.format = 'json'
+        req.url = newpath + '?' + querystring.stringify(query);
 
-      console.log('proxying', url, req.url);
+        console.log('proxying', url, req.url);
 
-      proxy.on('proxyRes', function (res) {
-        // aggro cache, see https://devcenter.heroku.com/articles/increasing-application-performance-with-http-cache-headers
-        res.headers['Cache-Control'] = 'public, max-age=31536000';
-        res.headers['Expires'] = 'Mon, 25 Jun 2015 21:31:12 GMT';
-      });
+        proxy.on('proxyRes', function (res) {
+          // aggro cache, see https://devcenter.heroku.com/articles/increasing-application-performance-with-http-cache-headers
+          res.headers['Cache-Control'] = 'public, max-age=31536000';
+          res.headers['Expires'] = 'Mon, 25 Jun 2015 21:31:12 GMT';
+        });
 
-      proxy.web(req, res, {
-        target: 'http://api.rovicorp.com',
-        headers: {
-          port: 80,
-          host: 'api.rovicorp.com'
-        }
-      });
-    } else {
-      next();
-    }
+        proxy.web(req, res, {
+          target: 'http://api.rovicorp.com',
+          headers: {
+            port: 80,
+            host: 'api.rovicorp.com'
+          }
+        });
+      } else {
+        next();
+      }
+    });
   }
 };
