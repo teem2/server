a3214db4d4f63f8a78534e5680569ad12c493c92
Max Carlson <max@maxcarlson.com>
2014-11-17 20:43:37 -0800

    Add auto reload support

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ var compress = require('compression')
 var exec = require('child_process').exec;
 var fs = require('fs');
 var proxy = require('express-http-proxy');
+var path = require('path')
 
 var app = express();
 var server = http.createServer(app);
@@ -62,9 +63,9 @@ app.use('/img/', proxy('cps-static.rovicorp.com', {
 }));
 
 if (process.env.DREEM_PROJECTS_ROOT) {
-    var projectsroot = __dirname + '/' + process.env.DREEM_PROJECTS_ROOT
-    console.log('serving project root from', projectsroot);
-    app.use('/projects', express.static(projectsroot));
+  var projectsroot = __dirname + '/' + process.env.DREEM_PROJECTS_ROOT
+  console.log('serving project root from', projectsroot);
+  app.use('/projects', express.static(projectsroot));
 }
 
 
@@ -81,58 +82,115 @@ var findErrors = function (parsererror) {
 }
 
 var validateXml = function (path, resultsCallback) {
-    exec("xmllint " + path, function(error, stdout, stderr) { 
-      var array = stderr.toString().split("\n");
-      var out = [];
-      for (var i = 0; i < array.length; i += 3) {
-        if (findErrors(array[i])) {
-          // console.log('skipping', array[i])
-        } else if (i + 3 < array.length) {
-          out = out.concat(array.slice(i, i + 3))
-          // console.log('preserving', array.slice(i, i + 3), out)
-        }
-      }
-      resultsCallback(out);
-    });
-}
-
-var validateXmlWindows = function (path, resultsCallback) {
-    exec("xmllint_windows " + path, function(error, stdout, stderr) { 
-      var array = stderr.toString().split("\n");
-      var out = [];
-      for (var i = 0; i < array.length; i ++) {
+  exec("xmllint " + path, function(error, stdout, stderr) { 
+    var array = stderr.toString().split("\n");
+    var out = [];
+    for (var i = 0; i < array.length; i += 3) {
+      if (findErrors(array[i])) {
+        // console.log('skipping', array[i])
+      } else if (i + 3 < array.length) {
         out = out.concat(array.slice(i, i + 3))
         // console.log('preserving', array.slice(i, i + 3), out)
       }
-      resultsCallback(out);
-    });
+    }
+    resultsCallback(out);
+  });
+}
+
+var validateXmlWindows = function (path, resultsCallback) {
+  exec("xmllint_windows " + path, function(error, stdout, stderr) { 
+    var array = stderr.toString().split("\n");
+    var out = [];
+    for (var i = 0; i < array.length; i ++) {
+      out = out.concat(array.slice(i, i + 3))
+      // console.log('preserving', array.slice(i, i + 3), out)
+    }
+    resultsCallback(out);
+  });
 }
 
 app.get(/^\/(validate).+/, function (req, res, next) {
-    var path = req.query.url.substring(1);
-    // handle project and root paths
-    if (path.indexOf('projects/') === 0){
-        path = projectsroot + path.substring(9);
-    } else {
-        path = dreemroot + path;
+  var path = req.query.url.substring(1);
+  // handle project and root paths
+  if (path.indexOf('projects/') === 0){
+    path = projectsroot + path.substring(9);
+  } else {
+    path = dreemroot + path;
+  }
+
+  if (path.lastIndexOf('/') === path.length - 1)
+    path += 'index.html';
+
+  // console.log('validating path', path)
+
+  var sendresults = function(results) {
+    // console.log(results);
+    res.writeHead(200, { 'Content-Type': 'application/json' }); 
+    res.end(JSON.stringify(results));
+  }
+
+  if (process.platform.indexOf('win32') >= 0) {
+    validateXmlWindows(path, sendresults);
+  } else {
+    validateXml(path, sendresults);
+  }
+});
+
+
+
+var watch = "mtime" // or use "ctime"
+var watchRes = []
+var root = path.resolve(__dirname).toString();
+var delta = 0
+var watching = {}
+var stats = {}
+var watchFile = function watchFile(filename, path){
+  if(watching[filename]) return
+  stats[filename] = fs.statSync(filename)[watch].toString()
+  // console.log('watching file',filename)
+  watching[filename] = setInterval(function(){
+    var stat = fs.statSync(filename)
+    var diff = 0
+    if(stat[watch].toString() != stats[filename]){ 
+      stats[filename] = stat[watch].toString()
+      if(Date.now() - delta > 2000){
+        delta = Date.now()
+        // console.log(path + " changed, sending reload to frontend ----")
+        for(var i in watchRes){
+          var res = watchRes[i]
+          res.writeHead(200, {"Content-Type": "text/plain"})
+          res.end(path)
+        }
+        watchRes = []
+        primus.write({filechange: path})
+      }
+    }
+  },50)
+}
+
+app.get(/^\/(watchfile).+/, function (req, res, next) {
+  var name = req.query.url.substring(1)
+  if (name === "_reloader_") {
+    watchRes.push(res)
+    return
+  }   
+  if (name.indexOf('projects/') === 0){
+    name = projectsroot + name.substring(9);
+  } else {
+    name = dreemroot + name;
+  }
+  fs.exists(name, function(x) {
+    if (! x) {
+      console.log('File not found:'+name)
+      res.writeHead(404)
+      res.end("file not found")
+      return
     }
 
-    if (path.lastIndexOf('/') === path.length - 1)
-        path += 'index.html';
-
-    // console.log('validating path', path)
-
-    var sendresults = function(results) {
-        // console.log(results);
-        res.writeHead(200, { 'Content-Type': 'application/json' }); 
-        res.end(JSON.stringify(results));
-    }
-
-    if (process.platform.indexOf('win32') >= 0) {
-        validateXmlWindows(path, sendresults);
-    } else {
-        validateXml(path, sendresults);
-    }
+    watchFile(name, req.query.url)
+    res.writeHead(200)
+    res.end()
+  });
 });
 
 var primus = new Primus(server, { transformer: 'SockJS'});
